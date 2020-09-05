31cc78c89360389ce1ac181bd2532e64df8b192d
afozbek <furkanozbek1995@gmail.com>
2020-09-05 14:24:05 +0300

    http istek methodu okunabilir hale getirildi

--- frontend/src/perfAnalytics.js
+++ frontend/src/perfAnalytics.js
@@ -1,3 +1,26 @@
+let basePostUrl = "http://localhost:8080/browser-metrics";
+
+function logMetrics({
+  measureName,
+  measureValue,
+  timestamp,
+  url = basePostUrl,
+}) {
+  fetch(url, {
+    method: "POST",
+    body: JSON.stringify({
+      timestamp,
+      measureValue,
+      measureName,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => console.log(data));
+}
+
 (function () {
   var logStyle =
     "color: green; font-weight: bold; font-size: 14px; text-transform: uppercase";
@@ -14,47 +37,63 @@
   // Window Load
   var loadTime;
 
+  var timestamp = new Date().valueOf();
+  logMetrics({
+    timestamp,
+    measureName: "ttfb",
+    measureValue: ttfb,
+  });
+
   var observer = new PerformanceObserver(function (list, obj) {
     var perfEntries = list.getEntries();
     // console.log(perfEntries);
-    for (var i = 0; i < perfEntries.length; i++) {
-      var entry = perfEntries[i];
-
-      if (entry.entryType === "navigation") {
-        console.log(entry.loadEventEnd);
+    for (let entry of perfEntries) {
+      const { entryType } = entry;
+      if (entryType === "navigation") {
         contentLoadTime = entry.domContentLoadedEventEnd;
         loadTime = entry.loadEventEnd;
-
-        console.log(`%c${entry.entryType}`, logStyle);
+        logMetrics({
+          timestamp,
+          measureName: "DOM Load Time",
+          measureValue: contentLoadTime,
+        });
+        logMetrics({
+          timestamp,
+          measureName: "Load Time",
+          measureValue: loadTime,
+        });
         console.log("%cDOM Load Time: ", logStyle, contentLoadTime + " ms");
         console.log("%cLoad Time: ", logStyle, loadTime + " ms");
-      } else if (entry.entryType === "paint") {
-        console.log(`%c${entry.name} :`, logStyle, entry.startTime + " ms");
+      } else if (
+        entryType === "paint" &&
+        entry.name === "first-contentful-paint"
+      ) {
         fcp = entry.startTime;
-      } else if (entry.entryType === "resource") {
-        console.log(`%c${entry.entryType}`, logStyle);
-        console.log(`${entry.name} : ` + entry.responseEnd);
+
+        logMetrics({
+          timestamp,
+          measureName: entry.name,
+          measureValue: fcp,
+        });
+        console.log(`%c${entry.name} :`, logStyle, entry.startTime + " ms");
+      } else if (entryType === "resource") {
+        logMetrics({
+          timestamp,
+          measureName: "Resource",
+          measureValue: entry.responseEnd,
+        });
+        console.log("Resource :" + entry.responseEnd);
       }
     }
   });
 
-  observer.observe({ entryTypes: ["paint", "navigation", "resource"] });
-
-  var basePostUrl = "http://localhost:8080/browser-metrics";
-  fetch(basePostUrl, {
-    method: "POST",
-    body: JSON.stringify({
-      message: "Furkan Ozbek",
-      fcp: fcp,
-      contentLoadTime: contentLoadTime,
-      loadTime: loadTime,
-    }),
-    headers: {
-      "Content-Type": "application/json",
-    },
-  })
-    .then((res) => res.json())
-    .then((data) => console.log(data));
-
+  observer.observe({
+    entryTypes: ["paint", "navigation"],
+    // type: "paint",
+    buffered: true,
+  });
+  setTimeout(() => {
+    observer.disconnect();
+  }, 10000);
   // Network Timings
 })();
