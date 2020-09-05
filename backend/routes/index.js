8c209e7cf54d636b8dc870c7245401d88afdfec6
afozbek <furkanozbek1995@gmail.com>
2020-09-05 14:23:44 +0300

    mongodb içerisinde bağlantılar yapıldı

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var router = express.Router();
 
+var Metric = require("../models/metrics");
+
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.status(200).json({
@@ -9,9 +11,26 @@ router.get("/", function (req, res, next) {
 });
 
 router.post("/browser-metrics", function (req, res, next) {
+  const { timestamp, measureValue, measureName } = req.body;
+
   console.log("receiving data ...");
   console.log("body is ", req.body);
-  res.send(req.body);
+  const newMetric = new Metric({
+    timestamp,
+    measureValue,
+    measureName,
+  });
+
+  newMetric
+    .save()
+    .then((result) => {
+      console.log(result);
+      res.send(req.body);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.send({ message: "Error Happened" });
+    });
 });
 
 module.exports = router;
